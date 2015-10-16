22e107c400f022206ad94722e012370dd0ed3d31
Jonathan Frederick <jrfreder@gmail.com>
2015-10-16 16:43:16 -0400

    pulling in some bookmarks from the mls site

---
+++ urlybird/bookmarkapp/migrations/0001_initial.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Bookmark',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
+                ('title', models.CharField(max_length=20)),
+                ('description', models.CharField(max_length=255, null=True)),
+                ('original_url', models.URLField()),
+                ('short_url', models.CharField(max_length=7)),
+                ('timestamp', models.DateTimeField(auto_now_add=True)),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Click',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
+                ('timestamp', models.DateTimeField(auto_now_add=True)),
+                ('bookmark', models.ForeignKey(to='bookmarkapp.Bookmark')),
+            ],
+        ),
+    ]
