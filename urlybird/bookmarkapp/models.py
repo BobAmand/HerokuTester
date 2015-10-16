3d9cf0434b149ff634d80128333b43254667e254
Bob Amand <biotmg@nc.rr.com>
2015-10-15 21:38:34 -0400

    Initial models.py draft

--- urlybird/bookmarkapp/models.py
+++ urlybird/bookmarkapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+'''
+Two models will be built.
+    - Bookmark will:
+        > capture the title + description of site bookmarked.
+        > note the original URL and translate to a short URL.
+        > note the time of selection (click) as a bookmark.
+        > link the data to a User.
+    - Click will:
+        > time stamp each time the bookmark is used by user.
+'''
+
+
+class Bookmark()
