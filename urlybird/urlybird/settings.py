a2aa861691b294e88ef67f58c31b7aed9bd093be
Bob Amand <biotmg@nc.rr.com>
2015-10-15 20:57:45 -0400

    Replaced 'click' app with 'bookmarkapp' in master branch

--- urlybird/urlybird/settings.py
+++ urlybird/urlybird/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'click',
-)
+    'bookmarkapp'
+    )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
