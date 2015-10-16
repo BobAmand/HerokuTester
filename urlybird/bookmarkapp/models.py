0165e1cbae58d11a97750eda8ab9ba3dd010d95d
Bob Amand <biotmg@nc.rr.com>
2015-10-15 23:18:14 -0400

    draft attributes in models.py

--- urlybird/bookmarkapp/models.py
+++ urlybird/bookmarkapp/models.py
@@ -6,11 +6,39 @@ Two models will be built.
     - Bookmark will:
         > capture the title + description of site bookmarked.
         > note the original URL and translate to a short URL.
-        > note the time of selection (click) as a bookmark.
+        > note the time of bookmark selection (click).
+         >> distinguish between initial and return?
+         >> distinquish between adding and deleting?
         > link the data to a User.
     - Click will:
         > time stamp each time the bookmark is used by user.
+        > many clicks for one user.
+        > many clicks for many users.
 '''
 
 
-class Bookmark()
+class Bookmark(models.Model):
+
+    title = models.CharField(max_length=20)
+    description = models.CharField(max_length=255)
+    original_url = models.URLField()  # max_length is 200 default.
+    short_url = models.CharField(max_length=7)  # assuming 7 characters
+    # timestamp = models.DateTimeField(auto_now_add=True)  # save every click
+    # user = models.ForeignKey(Click)
+
+
+class Click(models.Model):
+
+    # ADD = 'A'
+    # DEL = 'D'
+    # USE = 'U'
+    # ACTIVITY_SELECT = (
+    #     (ADD, 'A'),
+    #     (DEL, 'D'),
+    #     (USE, 'U'),
+    # )
+    #
+    # activty = models.CharField(max_length=1, choices=ACTIVITY_SELECT)
+
+    timestamp = models.DateTimeField(auto_now_add=True)  # save every click
+    user = models.ForeignKey(Bookmark)
