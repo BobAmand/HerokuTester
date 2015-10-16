f90a2d34590257cd33f4bf9b0aa7a52922aa50f0
Jonathan Frederick <jrfreder@gmail.com>
2015-10-16 12:37:44 -0400

    logging in upon registration

--- urlybird/bookmarkapp/views.py
+++ urlybird/bookmarkapp/views.py
@@ -9,8 +9,31 @@ from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 
-def registration():
-    pass
+def registration(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            password = user.password
+
+            user.set_password(password)
+            user.save()
+
+            profile = Profile(
+                user=user,
+                favorite_color='blue',
+            )
+            profile.save()
+
+            user = authenticate(username=user.username,
+                                password=password)
+            login(request, user)
+            return redirect('recent_statuses')
+    else:
+        form = UserForm()
+    return render(request, 'registration/register.html',
+                  {'form': form})
 
 
 class UserCreateView(FormView):
@@ -19,8 +42,8 @@ class UserCreateView(FormView):
     success_url = '/register'
 
     def form_valid(self, form):
-        form.save()
-        # user = authenticate(username=self.request.user.username,
-        #                     password=self.request.user.password)
-        # login(self.request, user)
+        new_user = form.save()
+        user = authenticate(username=new_user.username,
+                            password=form.cleaned_data['password1'])
+        login(self.request, user)
         return super(UserCreateView, self).form_valid(form)
