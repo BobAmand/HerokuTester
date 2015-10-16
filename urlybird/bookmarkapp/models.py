4f491d753160a78ad236656269328ddb3d3c7b36
Bob Amand <biotmg@nc.rr.com>
2015-10-16 11:18:18 -0400

    revised models

--- urlybird/bookmarkapp/models.py
+++ urlybird/bookmarkapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 '''
@@ -9,6 +10,7 @@ Two models will be built.
         > note the time of bookmark selection (click).
          >> distinguish between initial and return?
          >> distinquish between adding and deleting?
+         >> distinquish between logon + logoff (time on site)?
         > link the data to a User.
     - Click will:
         > time stamp each time the bookmark is used by user.
@@ -20,12 +22,14 @@ Two models will be built.
 class Bookmark(models.Model):
 
     title = models.CharField(max_length=20)
-    description = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, null=True)
     original_url = models.URLField()  # max_length is 200 default.
     short_url = models.CharField(max_length=7)  # assuming 7 characters
-    # timestamp = models.DateTimeField(auto_now_add=True)  # save every click
-    # user = models.ForeignKey(Click)
+    timestamp = models.DateTimeField(auto_now_add=True)  # save every click
+    author = models.ForeignKey(User)
 
+    def __str__(self):
+        return self.title
 
 class Click(models.Model):
 
@@ -41,4 +45,7 @@ class Click(models.Model):
     # activty = models.CharField(max_length=1, choices=ACTIVITY_SELECT)
 
     timestamp = models.DateTimeField(auto_now_add=True)  # save every click
-    user = models.ForeignKey(Bookmark)
+    bookmark = models.ForeignKey(Bookmark)
+
+
+class Clicker(models.Model):
