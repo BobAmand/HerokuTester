70a8e6980aa20bb098278f9c9879d7bdc1eed73a
Bob Amand <biotmg@nc.rr.com>
2015-10-18 11:43:44 -0400

    Last commit before working 'short url'

--- urlybird/bookmarkapp/forms.py
+++ urlybird/bookmarkapp/forms.py
@@ -11,20 +11,11 @@ class BookmarkForm(forms.Form):
                                   )     # removed null=True
     original_url = forms.URLField()  # max_length is 200 default.
 
-    short_url = forms.CharField(max_length=7)  # assuming 7 characters
-    timestamp = forms.DateTimeField(input_formats='%Y-%m-%d %H:%M:%S')
-                                    # removed 'auto_now_add=True'
+    # short_url = forms.CharField(max_length=7)  # assuming 7 characters
+    # timestamp = forms.DateTimeField(input_formats='%Y-%m-%d %H:%M:%S')
+    # removed 'auto_now_add=True' from timestamp
     # author = forms.ForeignKey(User)
-
-
 
     class Meta:
         model = Bookmark
-        fields = ('title', 'description', 'original_url', 'short_url',
-                  'timestamp', 'author')
-
-
-
-    # short_url = models.CharField(max_length=7)  # assuming 7 characters
-    # timestamp = models.DateTimeField(auto_now_add=True)  # save every click
-    # author = models.ForeignKey(User)
+        fields = ('title', 'description', 'original_url')
